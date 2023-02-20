435abb79635be769c519cc94dc7e41eae31d4dd2
PJK <>
2023-02-20 23:15:20 +0900

    Solve Q002 (1).

---
+++ src/Q002.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Q002 {
+    public static void main(String[] args){
+        Scanner input1 = new Scanner(System.in);
+        int theNumberOfSubjects = input1.nextInt();
+
+        Scanner input2 = new Scanner(System.in);
+        ArrayList scores = new ArrayList();
+
+    }
+}
