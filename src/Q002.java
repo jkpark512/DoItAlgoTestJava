84a724d0d91aa666538d40bebb7ab3cb17d31972
PJK <jk.park512@gmail.com>
2023-02-25 23:45:16 +0900

    Solve Q002 (3).

--- src/Q002.java
+++ src/Q002.java
@@ -1,6 +1,7 @@
 // 평균 구하기.
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Q002 {
@@ -9,7 +10,21 @@ public class Q002 {
         int theNumberOfSubjects = input1.nextInt();
 
         Scanner input2 = new Scanner(System.in);
-        ArrayList scores = new ArrayList();
+        Int[] array = new int[theNumberOfSubjects];
+
+        for(int i=0; i<array.length; i++){
+            array[i] = input2.nextInt();
+        }
+
+        Arrays.sort(array);
+
+        System.out.println(arrays);
+
+     /*   for(int i=0; i<array.length; i++){
+            array[array.length]
+        }
+
+    */
 
     }
 }
